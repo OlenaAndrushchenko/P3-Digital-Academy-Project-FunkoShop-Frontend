7c8b3087852e43ae66a91b0964ff4f222abf234f
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-27 14:23:36 +0200

    orders management table in process

--- src/views/admin/OrdersView.vue
+++ src/views/admin/OrdersView.vue
@@ -1,12 +1,13 @@
 <script setup>
 import AdminHeading from '@/components/AdminDashboard/AdminHeading.vue';
+import OrderTable from '@/components/AdminDashboard/AdminTables/OrderTable.vue';
 
 </script>
 
 <template>
     <div>
         <AdminHeading title="Orders Management" />
-
+        <OrderTable />
     </div>
 </template>
   
